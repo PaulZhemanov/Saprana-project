7fd62f5fb130f606037ed01fb33621c478f346c4
Paul <zhpv96@gmail.com>
2023-08-05 23:57:39 +0300

    buy ticket button added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,47 @@
-import './App.css';
-import { useFuel } from './hooks/useFuel';
-import { useIsConnected } from './hooks/useIsConnected';
-
+import "./App.css";
+import { useFuel } from "./hooks/useFuel";
+import { useIsConnected } from "./hooks/useIsConnected"; 
+import { Wallet } from "fuels";
 
 function App() {
   const [fuel, notDetected] = useFuel();
   const [isConnected] = useIsConnected();
+
+  const BuyTicket = () => {
+    // Ваша логика покупки билета...
+    console.log('Билет куплен!');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>nft ticketing</h1>
+      <header>
+        <h1>saprana</h1>
       </header>
-
 
       {fuel && (
         <div>
+          <div>fuel is detected</div>
           {isConnected ? (
             <div>
-              <button onClick={newPlayer}>New Player</button>
-              <button onClick={levelUp}>Level Up</button>
+              <div>you are connected!</div>
+              <button onClick={() => fuel.connect()}>Connect wallet</button>
+              <button onClick={BuyTicket}>Buy ticket</button>
             </div>
           ) : (
-            <button onClick={() => fuel.connect()}>Connect Wallet</button>
+            <button onClick={() => fuel.connect()}>Connect wallet</button>
           )}
         </div>
       )}
 
-      {notDetected && <div>fuel NOT detected.</div>}
+      {!fuel && <div>fuel not detected</div>}
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
