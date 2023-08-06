b04a9c5fd34ac39bfc10f0cfcd25b7d1d2a69172
Paul <zhpv96@gmail.com>
2023-08-06 14:21:19 +0300

    buy ticket button fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,27 @@
 import "./App.css";
 import { useFuel } from "./hooks/useFuel";
-import { useIsConnected } from "./hooks/useIsConnected"; 
-import { Wallet } from "fuels";
+import { useIsConnected } from "./hooks/useIsConnected";
 import { SapranaAbi__factory } from "./contracts";
 
-const CONTRACT_ID = "0x010e919ebe4c15517279e2d942cdff66ef28032f59b9d4b0001839ffa5201ffb";
-
-
+const CONTRACT_ID =
+  "0x010e919ebe4c15517279e2d942cdff66ef28032f59b9d4b0001839ffa5201ffb";
 
 function App() {
-  const [fuel, notDetected] = useFuel();
-  const contract = SapranaAbi__factory.connect(CONTRACT_ID, fuel);
+  const [fuel] = useFuel();
   const [isConnected] = useIsConnected();
-
-  const BuyTicket= () => {
-  };
-
-const CreateEvent = () => {
-};
+  const ticketPrice = 0.01 * 1e9;
+  const buyTicket = () =>
+    fuel
+      .currentAccount()
+      .then((account) => fuel.getWallet(account))
+      .then((wallet) => SapranaAbi__factory.connect(CONTRACT_ID, wallet))
+      .then((contract) =>
+        contract.functions
+          .buy_ticket(1)
+          .txParams({ gasPrice: 1 })
+          .callParams({ forward: { amount: ticketPrice } })
+          .call()
+      );
 
   return (
     <div className="App">
@@ -26,14 +30,12 @@ const CreateEvent = () => {
       </header>
 
       {fuel && (
-        <div> 
+        <div>
           <div>Wallet is detected</div>
           {isConnected ? (
             <div>
               <div>You are connected</div>
-              <button onClick={() => fuel.connect()}>Connect wallet</button>
-              <button onClick={BuyTicket}>Buy ticket</button>
-              <button onClick={CreateEvent}>Сreate Event</button>{" "}
+              <button onClick={buyTicket}>Buy ticket</button>
             </div>
           ) : (
             <button onClick={() => fuel.connect()}>Connect wallet</button>
@@ -47,9 +49,3 @@ const CreateEvent = () => {
 }
 
 export default App;
-
-
-
-
-
-
